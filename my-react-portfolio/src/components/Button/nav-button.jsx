daec041bf2d2a1d618589d2ae7c2c1bc9332d9d9
Escanor1986 <lionel.zovi@gmail.com>
2024-03-05 20:09:08 +0100

    Added new font (neon80s) - began with mobile navbar

--- my-react-portfolio/src/components/Button/nav-button.jsx
+++ my-react-portfolio/src/components/Button/nav-button.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 function NavButton(props) {
   return (
-    <li className="px-3 group">
+    <li className="navbar-li px-3 group">
       <a href={props.href} className="block text-center">
-        <button className="relative overflow-hidden p-4 bg-dark-bg border-2 border-rose-bg rounded-3xl shadow-lg transition-all duration-500 ease-in-out transform group-hover:scale-105">
+        <button className="navbar-button relative overflow-hidden p-4 bg-dark-bg border-2 border-rose-bg rounded-3xl shadow-lg transition-all duration-500 ease-in-out transform group-hover:scale-105">
           <span className="block relative z-10">{props.buttonText}</span>
           <div className="absolute inset-0 bg-rose-bg opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out"></div>
           <div className="absolute inset-0 z-0 w-full h-full transform rotate-45 translate-x-full bg-white opacity-20 group-hover:translate-x-0 transition-transform duration-700"></div>
--- my-react-portfolio/src/components/Navbar/navbar.jsx
+++ my-react-portfolio/src/components/Navbar/navbar.jsx
@@ -1,17 +1,28 @@
 import NavButton from '../Button/nav-button';
-import Logo from '../Logo/logo';
+// import { useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
+  // const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // const toggleMobileMenu = () => setMobileMenuOpen(!isMobileMenuOpen);
+
   return (
-    <nav className="flex justify-center px-8 py-4">
-      <ul className="flex justify-between items-center text-text-border-white text-3xl w-full">
-        <div className="flex">
-          <Logo href="#/" />
-        </div>
-        <div className="flex">
+    <nav className="mobile-navbar flex flex-col justify-center p-8">
+      {/*       <span
+        className="mobile-menu-icon text-rose-bg"
+        onClick={toggleMobileMenu}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </span> */}
+      <ul className="mobile-ul-navbar flex justify-between items-center text-text-border-white text-3xl w-full">
+        <div className="navbar-left flex">
           <NavButton href="#/" buttonText="Home" />
           <NavButton href="#/about" buttonText="About" />
           <NavButton href="#/projects" buttonText="Projects" />
+        </div>
+        <div className="navbar-right flex">
           <NavButton href="#/trainings" buttonText="Trainings" />
           <NavButton href="#/skills" buttonText="Skills" />
           <NavButton href="#/contact" buttonText="Contact" />
